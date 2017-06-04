c1bcb570af87638d15358f2ffa6cdb8f4fd18d09
Tsiruan <b03902079@ntu.edu.tw>
2017-06-05 06:11:00 +0800

    bluetooth discovery

--- app/src/main/java/tw/edu/ntu/csie/bluetooth/MainActivity.java
+++ app/src/main/java/tw/edu/ntu/csie/bluetooth/MainActivity.java
@@ -5,6 +5,10 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,30 +21,64 @@ import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends Activity  {
-    Button b1,b2,b3,b4;
+    Button b1,b2,b3,b4,b5;
     private BluetoothAdapter BA;
-    private Set<BluetoothDevice>pairedDevices;
+    private ArrayAdapter<String> mBTArrayAdapter;
+    private Set<BluetoothDevice> pairedDevices;
     ListView lv;
+
+    ArrayList list = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1 = (Button)findViewById(R.id.button);
-        b2 = (Button)findViewById(R.id.button2);
-        b3 = (Button)findViewById(R.id.button3);
-        b4 = (Button)findViewById(R.id.button4);
+        b1 = (Button)findViewById(R.id.bluetoothOn);
+        b2 = (Button)findViewById(R.id.bluetoothOff);
+        b3 = (Button)findViewById(R.id.listPaired);
+        b4 = (Button)findViewById(R.id.bluetoothVisible);
+        b5 = (Button)findViewById(R.id.discover);
+        lv = (ListView)findViewById(R.id.listView);
+        mBTArrayAdapter = new ArrayAdapter<String> (this,android.R.layout.simple_list_item_1);
+
 
         BA = BluetoothAdapter.getDefaultAdapter();
-        lv = (ListView)findViewById(R.id.listView);
 
-        if (BA == null)
-        {
-            System.out.println ("BluetoothAdapter is null");
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+
+        if (BA == null) {
+            // Device doesn't support bluetooth
+            Toast.makeText(getApplicationContext(), "Bluetooth unsupported", Toast.LENGTH_LONG).show();
         }
     }
 
+
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+
+                list.add(deviceName);
+                final ArrayAdapter adapter = new ArrayAdapter(context,android.R.layout.simple_list_item_1, list);
+                lv.setAdapter(adapter);
+            }
+        }
+    };
+
+
+
+    // turn on bluetooth
     public void on(View v){
         if (!BA.isEnabled()) {
             Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -51,22 +89,52 @@ public class MainActivity extends Activity  {
         }
     }
 
+    // turn off bluetooth
     public void off(View v){
         BA.disable();
         Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
     }
 
 
+    // make device visible
     public void visible(View v){
         Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
         startActivityForResult(getVisible, 0);
     }
 
 
+    // discover devices
+    public void discover(View v){
+        list.clear();
+        Toast.makeText(getApplicationContext(), "Showing Discovered Devices",Toast.LENGTH_SHORT).show();
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        lv.setAdapter(adapter);
+
+        // Check if the device is already discovering
+        if(BA.isDiscovering()){
+            BA.cancelDiscovery();
+            Toast.makeText(getApplicationContext(),"Discovery stopped",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            if(BA.isEnabled()) {
+                mBTArrayAdapter.clear(); // clear items
+                BA.startDiscovery();
+                Toast.makeText(getApplicationContext(), "Discovery start", Toast.LENGTH_SHORT).show();
+                registerReceiver(mReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    // list paired devices
     public void list(View v){
         pairedDevices = BA.getBondedDevices();
 
-        ArrayList list = new ArrayList();
+        //ArrayList list = new ArrayList();
+        list.clear();
 
         for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
         Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
@@ -74,5 +142,14 @@ public class MainActivity extends Activity  {
         final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
 
         lv.setAdapter(adapter);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // unregister the ACTION_FOUND receiver.
+        unregisterReceiver(mReceiver);
     }
 }
